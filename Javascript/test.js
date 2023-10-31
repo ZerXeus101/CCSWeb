48e5b914d6148043e5573eca0e6f99643af09140
ZerXeus101 <joshuakgbriones@gmail.com>
2023-10-31 22:04:28 +0800

    updated some stuff
    some of the files were moved and changed for better scaling

---
+++ Javascript/test.js
@@ -0,0 +1,43 @@
+var currentTheme = 1;
+
+    // *list of themes:
+    // *1. default
+    // *2. autumn
+    // *3. summer
+    // *4. Achromatic
+    // *5. Cyber Punk
+    
+console.log(currentTheme);
+function defaultTheme() {
+  if (currentTheme == 2 || currentTheme == 3 || currentTheme == 4 || currentTheme == 5) {
+    document.getElementById("theme").className = "default";
+    console.log(currentTheme);
+  }
+}
+function autumnTheme() {
+    if (currentTheme == 1 || currentTheme == 3 || currentTheme == 4 || currentTheme == 5) {
+      document.getElementById("theme").className = "autumn";
+      currentTheme = 2;
+    }
+}
+
+function summerTheme() {
+    if (currentTheme == 1 || currentTheme == 2 || currentTheme == 4 || currentTheme == 5) {
+      document.getElementById("theme").className = "summer";
+      currentTheme = 3;
+    }
+}
+
+function achromaticTheme() {
+    if (currentTheme == 1 || currentTheme == 2 || currentTheme == 3 || currentTheme == 5) {
+      document.getElementById("theme").className = "achromatic";
+      currentTheme = 4;
+    }
+}
+
+function cyberpunkTheme() {
+    if (currentTheme == 1 || currentTheme == 2 || currentTheme == 3 || currentTheme == 4) {
+      document.getElementById("theme").className = "cyberpunk";
+      currentTheme = 5;
+    }
+}
