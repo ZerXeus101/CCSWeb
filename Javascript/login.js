fb7e9928f875d39f092e579cb51f75927fa878e8
ZerXeus101 <118508434+ZerXeus101@users.noreply.github.com>
2023-11-19 20:00:34 +0800

    added userType feature

--- Javascript/login.js
+++ Javascript/login.js
@@ -7,10 +7,70 @@ function request(form) {
     var password = form.pass.value;
     var userLength = form.user.value.trim().length;
     var passLength = form.pass.value.trim().length;
+    
     attempt--;
-    handleError(userLength, passLength, attempt, username, password);
-    handleAttempt(attempt);
-    handleLogIn(username,password);
+
+    var userType = form.userType.value;
+
+    console.log(userType);
+
+    if (userType == "1") {
+        handleError(userLength, passLength, attempt, username, password);
+        handleAttempt(attempt);
+        handleLogIn(username,password);
+    }
+    else if (userType == "2") {
+        let containsNonNumber = false;
+
+        for (let i = 0; i < password.length; i++) {
+        if (isNaN(password[i])) {
+            containsNonNumber = true;
+            break;
+            }
+        }
+    
+        if (containsNonNumber) {
+            password = false;
+        } else {
+            password = true;
+        }
+
+        handleErrorForStudent(userLength, passLength, attempt, username, password);
+        handleAttempt(attempt);
+        handleLogInForStudent(username,password);
+    }
+    else if (userType == "3") {
+        let containsNumber = false;
+        let containsUpperCase = false;
+
+        for (let i = 0; i < password.length; i++) {
+            if (Number(password[i])) {
+                containsNumber = true;
+                break;
+            }
+        }
+    
+        for (let i = 0; i < password.length; i++) {
+            if (password[i] !== password[i].toLowerCase()) {
+                containsUpperCase = true;
+                break;
+            }
+        }
+
+        if (containsUpperCase) {
+            console.log(containsUpperCase);
+        }
+
+        if (containsNumber && containsUpperCase) {
+            password = true;
+        } else {
+            password = false;
+        }
+
+        handleErrorForFaculty(userLength, passLength, attempt, username, password);
+        handleAttempt(attempt);
+        handleLogInForFaculty(username,password);
+    }
 }
 
 function handleAttempt(attempt) {
@@ -27,7 +87,7 @@ function handleAttempt(attempt) {
 	}
 }
 
-function handleError(userLength, passLength, attempt, username, password) {
+function handleError (userLength, passLength, attempt, username, password) {
     if (attempt == 0 || attempt < 0) {
         document.getElementById("error1").style.display = "none";
         document.getElementById("error2").style.display = "none";
@@ -54,6 +114,97 @@ function handleLogIn(username, password) {
         document.getElementById("error2").style.display = "none";
         document.getElementById("error3").style.display = "none";
         document.getElementById("error4").style.display = "none";
+        document.getElementById("count").style.display = "none";
+        alert("Welcome Joshua Briones! You have successfully logged in!");
+        attempt = attempt;
+        window.location = "index.html";
+    }
+}
+
+
+function handleUserType(menu, form) {
+    var userType = menu.value;
+    if (userType == "1") {
+        document.getElementById("input--button-login").disabled = false;
+        form.user.value = "";
+        form.pass.value = "";
+    }
+    else if (userType == "2") {
+        document.getElementById("input--field-pass").setAttribute("maxlength", "6");
+        form.user.value = "";
+        form.pass.value = "";
+        document.getElementById("input--button-login").disabled = false;
+    }
+    else if (userType == "3") {
+        userType = "faculty";
+        document.getElementById("input--field-pass").setAttribute("maxlength", "6");
+        form.user.value = "";
+        form.pass.value = "";
+        document.getElementById("input--button-login").disabled = false;
+    }
+}
+
+function handleErrorForStudent (userLength, passLength, attempt, username, password) {
+    if (attempt == 0 || attempt < 0) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "none";
+        document.getElementById("error3").style.display = "flex";
+    }
+    else if (userLength == 0 || passLength == 0){
+        document.getElementById("error1").style.display = "flex";
+    }
+    else if (userLength < 5 || passLength < 5) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "flex";
+    }
+    else if (username != adminUserName || !password) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "none";
+        document.getElementById("error3").style.display = "none";
+        document.getElementById("error5").style.display = "flex";
+    }
+}
+
+function handleLogInForStudent(username, password) {
+    if (username == adminUserName && password) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "none";
+        document.getElementById("error3").style.display = "none";
+        document.getElementById("error5").style.display = "none";
+        document.getElementById("count").style.display = "none";
+        alert("Welcome Joshua Briones! You have successfully logged in!");
+        attempt = attempt;
+        window.location = "index.html";
+    }
+}
+
+function handleErrorForFaculty (userLength, passLength, attempt, username, password) {
+    if (attempt == 0 || attempt < 0) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "none";
+        document.getElementById("error3").style.display = "flex";
+    }
+    else if (userLength == 0 || passLength == 0){
+        document.getElementById("error1").style.display = "flex";
+    }
+    else if (userLength < 5 || passLength < 5) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "flex";
+    }
+    else if (username != adminUserName || !password) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "none";
+        document.getElementById("error3").style.display = "none";
+        document.getElementById("error6").style.display = "flex";
+    }
+}
+
+function handleLogInForFaculty(username, password) {
+    if (username == adminUserName && password) {
+        document.getElementById("error1").style.display = "none";
+        document.getElementById("error2").style.display = "none";
+        document.getElementById("error3").style.display = "none";
+        document.getElementById("error6").style.display = "none";
         document.getElementById("count").style.display = "none";
         alert("Welcome Joshua Briones! You have successfully logged in!");
         attempt = attempt;
